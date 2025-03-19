c6ea686d2700e9c63a60f0815e47d3577daec721
Anze Peternel <peternelanze@gmail.com>
2025-03-19 02:46:02 +0100

    New interface method

--- internal/service/counter/interface.go
+++ internal/service/counter/interface.go
@@ -8,16 +8,14 @@ type ICounter interface {
 	Increment(incrementBy int) error
 	// Count the number of times a word is found in the file
 	countWord(filePath string) error
-
 	// Reset the counter
 	Reset()
-
 	// Process a file and send error to channel if any
 	processFile(path string, ch chan<- error)
-
 	// Process a directory and send error to channel if any
 	processDirectory(path string, ch chan<- error)
-
+	// Helper function to run tasks with worker pool
+	runTask(task func())
 	// Count the number of times a word is found starting from the root directory recursively
 	Count() <-chan error
 }
