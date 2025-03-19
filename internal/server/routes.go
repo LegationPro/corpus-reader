9eb567c49f5379a08aa871ac83861b71dff65a45
Anze Peternel <peternelanze@gmail.com>
2025-03-19 06:13:18 +0100

    Removed the / route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,14 +1,8 @@
 package server
 
-import "net/http"
-
 /*
 Setup and initialize the routes for the server
 */
 func (s *Server) SetupRoutes(handler *Handler) {
-	s.handler.HandleFunc("POST /counter", handler.handleCounter)
-
-	s.handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
-	})
+	s.handler.HandleFunc("POST /counter", handler.HandleCounter)
 }
