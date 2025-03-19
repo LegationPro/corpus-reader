280777654ed8eb68274346d6bd0b5e2908e1228d
Anze Peternel <peternelanze@gmail.com>
2025-03-19 06:10:58 +0100

    Addditional error checking

--- tests/counter_test.go
+++ tests/counter_test.go
@@ -1,16 +1,22 @@
 package tests
 
 import (
+	"os"
 	"testing"
 
 	"github.com/LegationPro/corpus-reader/internal/service/counter"
 )
 
-func TestCounterService(t *testing.T) {
+func Test_CounterService(t *testing.T) {
 	// CLI flag arguments
 	rootDir := "../corpus"
 	word := "john"
 	maxWorkers := 10
+
+	// Check if the directory exists
+	if _, err := os.Stat(rootDir); os.IsNotExist(err) {
+		t.Fatalf("directory %s does not exist", rootDir)
+	}
 
 	counterService := counter.New(word, rootDir, maxWorkers)
 	errChan := counterService.Count()
@@ -27,5 +33,7 @@ func TestCounterService(t *testing.T) {
 
 	if actualCount != uint64(expectedCount) {
 		t.Errorf("expected count %d, got %d", expectedCount, actualCount)
+	} else {
+		t.Logf("Successfully counted word occurrences in directory: %s", rootDir)
 	}
 }
