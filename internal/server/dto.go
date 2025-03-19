d1308a3adf3077f078afe14ce7b12176230a56b7
Anze Peternel <peternelanze@gmail.com>
2025-03-19 05:31:09 +0100

    Dtos for the server request-response cycle

---
+++ internal/server/dto.go
@@ -0,0 +1,19 @@
+package server
+
+// DTO (Data-Transfer-Object) for counter request
+// In a production environment, it could be enhanced using the validator package.
+type CounterRequest struct {
+	Directory string `json:"directory"`
+	Word      string `json:"word"`
+}
+
+// DTO (Data-Transfer-Object) for counter response
+type CounterResponse struct {
+	Count     int    `json:"count"`
+	Directory string `json:"directory"`
+}
+
+// DTO (Data-Transfer-Object) for error response
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
