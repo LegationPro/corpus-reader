3aae4f0f289c6dcbc13a7b7d2bae0771604452d2
Anze Peternel <peternelanze@gmail.com>
2025-03-19 04:25:11 +0100

    Moved to cmd/cli

---
+++ cmd/cli/cli.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/LegationPro/corpus-reader/internal/cli"
+	"github.com/LegationPro/corpus-reader/internal/service/counter"
+)
+
+func main() {
+	parsedArgs, err := cli.ParseFlags()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	counterService := counter.New(parsedArgs.Word, parsedArgs.Dir, parsedArgs.MaxWorkers)
+	errChan := counterService.Count()
+
+	// Read errors from the channel
+	for err := range errChan {
+		if err != nil {
+			log.Printf("Error occurred: %v", err)
+		}
+	}
+
+	if err != nil {
+		log.Fatalf("error when counting word: %v", err)
+	}
+
+	log.Printf("Word '%s' found %d times\n", parsedArgs.Word, counterService.GetCount())
+}
